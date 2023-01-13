656762a8efcbbe260efdb54d3e4907d83d4b702d
funkykong1 <vilho.valkeala@gmail.com>
2023-01-13 16:47:30 +0200

    added patrol points

---
+++ Assets/Scripts/PointManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+
+    // points where the bad guys go upon spawning
+    // numbered left to right
+    public GameObject[] HighPoints;
+    public GameObject[] MidPoints;
+    public GameObject[] LowPoints;
+    public GameObject[] BossPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
