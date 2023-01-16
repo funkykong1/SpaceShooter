cf10906f0380febe5745bcaae635e1458ee53a50
funkykong1 <vilho.valkeala@gmail.com>
2023-01-16 18:28:28 +0200

    scroll + blue sky
    the sun is shining in the sky
    there aint a cloud in sight

--- Assets/Scripts/BgScroll.cs
+++ Assets/Scripts/BgScroll.cs
@@ -7,16 +7,24 @@ public class BgScroll : MonoBehaviour
 
 public float scrollSpeed = 0.1f;
 
+private MeshRenderer mesh;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        mesh = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Scroll();
+    }
+
+    void Scroll()
+    {
+        x_Scroll = Time.time * scrollSpeed;
+
+        Vector2 offset = new Vector2(x_Scroll, 0f);
+        mesh.sharedMaterial.SetTextureOffset("_MainTex", offset);
     }
 }
