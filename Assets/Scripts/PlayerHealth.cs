a2105d39a20b465060d5045f45f873724bb9d845
funkykong1 <vilho.valkeala@gmail.com>
2023-02-03 18:08:14 +0200

    hp bar colors working goodly

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,17 +7,24 @@ public class PlayerHealth : MonoBehaviour
 {
     public float health;
     public float maxHealth;
+
     public Image healthBar;
+    public Gradient gradient;
 
     // Start is called before the first frame update
     void Start()
     {
         maxHealth = health;
+
+        healthBar.color = gradient.Evaluate(1f);
     }
 
     // Update is called once per frame
     void Update()
     {
         healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
+
+        //0 is empty and 1 is compeltely full, therefore we divide by the amount of health in the plane
+        healthBar.color = gradient.Evaluate(health / 100);
     }
 }
