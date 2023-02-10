5fb8f24d1d3819ddaa22a98cc60a4b88dfeb0fc8
funkykong1 <vilho.valkeala@gmail.com>
2023-02-10 18:18:11 +0200

    enemy positions better

--- Assets/Scripts/PointScript.cs
+++ Assets/Scripts/PointScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpriteBegone : MonoBehaviour
+public class PointScript : MonoBehaviour
 {
     private SpriteRenderer rend;
 
