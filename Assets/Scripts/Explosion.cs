f1ebb37df9bf97e62fbc8c84d029f20f32b76d0d
funkykong1 <vilho.valkeala@gmail.com>
2023-05-18 19:02:30 +0300

    more

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BeamExplosion : MonoBehaviour
+public class Explosion : MonoBehaviour
 {
 
     public void End()
